5871a321089fd6af622b85169d687627bb784f59
ernest-24 <volodyasaharevych@ukr.net>
2017-12-04 16:49:00 +0200

    [ change ] configs

--- app/configs/index.js
+++ app/configs/index.js
@@ -1,8 +1,3 @@
 module.exports = function( moduleApp ) {
     require('./_rout.js')( moduleApp );
-<<<<<<< HEAD
-    // require('./_translate.js')( moduleApp );
-  };
-=======
 };
->>>>>>> development
